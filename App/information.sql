12fe548a94f665a6de99b99172c63b0214abdebb
Арсений Каплин <kaplin_arseny@mail.ru>
2021-08-23 02:28:07 +0300

    refactor: updated requirements and files restructured

--- App/information.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS points;
-
-CREATE TABLE points (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    place TEXT NOT NULL,
-    amount INTEGER NOT NULL,
-    name TEXT NOT NULL,
-    func TEXT NOT NULL
-);
